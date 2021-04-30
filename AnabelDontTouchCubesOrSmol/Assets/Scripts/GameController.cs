4468beefad1d2b51c68e7746fff796b525dacacf
Code Ninjas 4 <codeninjassouthjordan@gmail.com>
2021-04-30 11:49:55 -0600

    v1.0
    still v1.0

--- AnabelDontTouchCubesOrSmol/Assets/Scripts/GameController.cs
+++ AnabelDontTouchCubesOrSmol/Assets/Scripts/GameController.cs
@@ -35,4 +35,8 @@ public class GameController : MonoBehaviour
         gameOverCanvas.SetActive(true);
         Time.timeScale = 0;
     }*/
+    /*public void Why()
+    {
+        //GameOver();
+    }*/
 }
